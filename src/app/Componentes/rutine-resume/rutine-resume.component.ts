3fa9df53b145cd45f330cec042c03c341d9ed7f8
ManuelZamora <rocker2503@gmail.com>
2022-11-24 19:26:48 -0600

    Componentes para la pagina

---
+++ src/app/Componentes/rutine-resume/rutine-resume.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rutine-resume',
+  templateUrl: './rutine-resume.component.html',
+  styleUrls: ['./rutine-resume.component.css']
+})
+export class RutineResumeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './Componentes/main-page/main-page.component';
+import { NavbarComponent } from './Componentes/navbar/navbar.component';
+import { LoginComponent } from './Componentes/login/login.component';
+import { RegisterComponent } from './Componentes/register/register.component';
+import { UserProfileComponent } from './Componentes/user-profile/user-profile.component';
+import { RutineResumeComponent } from './Componentes/rutine-resume/rutine-resume.component';
+import { RutineComponent } from './Componentes/rutine/rutine.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainPageComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserProfileComponent,
+    RutineResumeComponent,
+    RutineComponent,
   ],
   imports: [
     BrowserModule,
