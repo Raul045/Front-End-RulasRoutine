8a434eb9110989be048057226933c14ca4128fce
ManuelZamora <rocker2503@gmail.com>
2022-12-01 16:03:43 -0600

    feature/Vista - resumen de rutina terminada

---
+++ src/app/Componentes/table-usuarios/table-usuarios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-usuarios',
+  templateUrl: './table-usuarios.component.html',
+  styleUrls: ['./table-usuarios.component.css']
+})
+export class TableUsuariosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import { MainPageComponent } from './Componentes/main-page/main-page.component';
 import { RegisterComponent } from './Componentes/register/register.component';
 import { LoginComponent } from './Componentes/login/login.component';
 import { MakeRutineComponent } from './Componentes/make-rutine/make-rutine.component';
+import { RutineResumeComponent } from './Componentes/rutine-resume/rutine-resume.component';
 
 const routes: Routes = [
   {
     path: 'crear-rutina',
     component: MakeRutineComponent
+  },
+  {
+    path: 'rutina',
+    component: RutineResumeComponent
   },
   {
     path: 'blog',
