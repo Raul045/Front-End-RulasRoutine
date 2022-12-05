e09e2d1ca71efd2436eba8255425456b42243586
Angel Valdez <ang_endless27@hotmail.com>
2022-12-04 19:31:10 -0600

    [ADD] partial services

--- src/app/services/calorias.service.ts
+++ src/app/services/calorias.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment.prod";
+import { Calorias } from '../Models/calorias';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CaloriasService {
+  private URL_API = environment.apiURL
+  private entity = 'calorias'
 
-  constructor() { }
+  constructor(private http:HttpClient){}
+
+
+  getCalorias():Observable<Calorias>{
+      return this.http.get<Calorias>(this.URL_API+this.entity)
+  }
+
+  postCalorias(calorias:Calorias):Observable<Calorias>{
+      return this.http.post<Calorias>(this.URL_API+this.entity,calorias)
+  }
+
+  uptadeCalorias(calorias:Calorias,id:number):Observable<Calorias>{
+      return this.http.put<Calorias>(this.URL_API+this.entity+"/"+id,calorias)
+  }
+
 }
--- src/app/services/cuerpos.service.ts
+++ src/app/services/cuerpos.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment.prod";
+import { Cuerpos } from '../Models/cuerpos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CuerposService {
 
-  constructor() { }
+  private URL_API = environment.apiURL
+  private entity="cuerpos"
+  constructor(private http:HttpClient) { }
+
+  getCuerpos():Observable<Cuerpos>{
+    return this.http.get<Cuerpos>(this.URL_API)
+  }
+
+  addCuerpos(cuerpos:Cuerpos):Observable<Cuerpos>{
+    return this.http.post<Cuerpos>(`${this.URL_API}${this.entity}`,cuerpos);
+  }
+
+  updateCuerpos(id:number,cuerpos:Cuerpos):Observable<Cuerpos>{
+    return this.http.put<Cuerpos>(`${this.URL_API}${this.entity}/${id}`,cuerpos)
+  }
 }
