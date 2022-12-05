cc6e2a97928a82698106d3884f65e22b9dac0e47
Angel Valdez <ang_endless27@hotmail.com>
2022-12-04 21:07:03 -0600

    [ADD] partial services

--- src/app/services/calorias.service.ts
+++ src/app/services/calorias.service.ts
@@ -8,22 +8,24 @@ import { Calorias } from '../Models/calorias';
   providedIn: 'root'
 })
 export class CaloriasService {
-  private URL_API = environment.apiURL
-  private entity = 'calorias'
+  private URL_API = environment.apiURL+"/calorias"
+  private update = "/update"
+  private add_calories = '/add_calories'
+  private get_all_user = "/get_all_user"
 
   constructor(private http:HttpClient){}
 
 
   getCalorias():Observable<Calorias>{
-      return this.http.get<Calorias>(this.URL_API+this.entity)
+      return this.http.get<Calorias>(this.URL_API+this.get_all_user)
   }
 
   postCalorias(calorias:Calorias):Observable<Calorias>{
-      return this.http.post<Calorias>(this.URL_API+this.entity,calorias)
+      return this.http.post<Calorias>(this.URL_API+this.add_calories,calorias)
   }
 
   uptadeCalorias(calorias:Calorias,id:number):Observable<Calorias>{
-      return this.http.put<Calorias>(this.URL_API+this.entity+"/"+id,calorias)
+      return this.http.put<Calorias>(this.URL_API+this.update+"/"+id,calorias)
   }
 
 }
--- src/app/services/ejercicios.service.ts
+++ src/app/services/ejercicios.service.ts
@@ -9,19 +9,41 @@ import { Ejercicios } from '../Models/ejercicios';
 })
 export class EjerciciosService {
 
-  private URL_API = environment.apiURL
-  private entity="ejercicios"
+  private URL_API = environment.apiURL+"/ejercicios"
+  private get_all="/get_all"
+  private add_exercise="/add_exercise"
+  private update_ejercise="/update_ejercise"
+  private enable="/enable"
+  private disable="/disable"
+  private get_all_enabled="/get_all_enabled"
+  private get_all_disable="/get_all_disabled"
   constructor(private http:HttpClient) { }
 
   getEjercicios():Observable<Ejercicios>{
-    return this.http.get<Ejercicios>(this.URL_API)
+    return this.http.get<Ejercicios>(this.URL_API+this.get_all)
+  }
+
+  getEnableEjercicios():Observable<Ejercicios>{
+    return this.http.get<Ejercicios>(this.URL_API+this.get_all_enabled)
+  }
+
+  getDisabledEjercicios():Observable<Ejercicios>{
+    return this.http.get<Ejercicios>(this.URL_API+this.get_all_disable)
   }
 
   addEjercicios(ejercicios:Ejercicios):Observable<Ejercicios>{
-    return this.http.post<Ejercicios>(`${this.URL_API}${this.entity}`,ejercicios);
+    return this.http.post<Ejercicios>(`${this.URL_API}${this.add_exercise}`,ejercicios);
   }
 
   updateEjercicios(id:number,ejercicios:Ejercicios):Observable<Ejercicios>{
-    return this.http.put<Ejercicios>(`${this.URL_API}${this.entity}/${id}`,ejercicios)
+    return this.http.put<Ejercicios>(`${this.URL_API}${this.update_ejercise}/${id}`,ejercicios)
+  }
+
+  enableEjercicios(id:number,ejercicios:Ejercicios):Observable<Ejercicios>{
+    return this.http.put<Ejercicios>(`${this.URL_API}${this.enable}/${id}`,ejercicios)
+  }
+
+  disableEjercicios(id:number,ejercicios:Ejercicios):Observable<Ejercicios>{
+    return this.http.put<Ejercicios>(`${this.URL_API}${this.disable}/${id}`,ejercicios)
   }
 }
